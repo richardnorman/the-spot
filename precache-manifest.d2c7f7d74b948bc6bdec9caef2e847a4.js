fcf7fb831d91287cf80cc132a8e94021127d9ee3
Richard Norman <richard.norman.id@gmail.com>
2020-08-19 23:26:29 -0600

    Updates

---
+++ precache-manifest.d2c7f7d74b948bc6bdec9caef2e847a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb29fa930d57a9c86fb1e62778882538",
+    "url": "/the-spot/index.html"
+  },
+  {
+    "revision": "57b37fd894b4c57323c3",
+    "url": "/the-spot/static/css/main.b40ed939.chunk.css"
+  },
+  {
+    "revision": "b73cfba695950eb4fcf4",
+    "url": "/the-spot/static/js/2.1051b5ac.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/the-spot/static/js/2.1051b5ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57b37fd894b4c57323c3",
+    "url": "/the-spot/static/js/main.fd30a644.chunk.js"
+  },
+  {
+    "revision": "673d92431834fb860a45",
+    "url": "/the-spot/static/js/runtime-main.f148e1ad.js"
+  }
+]);
