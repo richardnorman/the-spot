dbc161f8f711d79e65d90cfa5d7d930241ab22cf
Richard Norman <richard.norman.id@gmail.com>
2020-09-22 15:00:19 -0600

    reconstructed App.js to be just a router

--- src/App.js
+++ src/App.js
@@ -1,71 +1,22 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import SpotCard from './components/SpotCard/SpotCard';
-import AddSpotButton from './components/AddSpotButton/AddSpotButton';
-import NavBar from './components/NavBar/NavBar';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
-  useHistory
 } from 'react-router-dom';
 import AddSpot from './components/AddSpot/AddSpot';
-import { useSelector, useDispatch } from 'react-redux';
 import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
 import UpdateSpot from './components/UpdateSpot/UpdateSpot';
-import { setSpotList } from './actions';
+import Spots from './components/Spots/Spots';
 
 export default function App() {
-  const history = useHistory();
-  const dispatch = useDispatch();
-  const spotList = useSelector(state => state.modifySpotList.spotList);
-  const searchText = useSelector(state => state.changeSearch.searchText);
-  const currentUser = useSelector(state => state.changeCurrentUser.currentUserEmail);
-
-  useEffect(() => {
-    if(!currentUser) {
-      history.push('/the-spot/sign-in')
-      return;
-    }
-    fetch(`https://the-spot01.herokuapp.com/get-spots/${currentUser}`)
-    .then(response => response.json())
-    .then(data => {
-      dispatch(setSpotList(data));
-    })
-    .catch(err => console.log(err))
-  })
-
-  const DisplaySpots = props => {
-    if(props.list.length === 0) {
-      return <p style={{textAlign: 'center', marginTop: '15%', fontSize: '1.5rem'}}>No spots found, <Link to='/the-spot/add-spot' style={{color: 'black'}}><strong>add one now!</strong></Link></p>
-    } else if(props.list.length > 0) {
-      return props.list.map(spot => {
-        return (
-            <SpotCard id={spot.id} title={spot.title} description={spot.description} image={spot.image} coords={spot.coords} dateCreated={spot.dateCreated}/>
-        );
-      })
-    } else {
-      return null;
-    }
-  }
     return (
       <Router>
-        <div>
           <Switch>
             <Route exact path='/the-spot'>
-              <NavBar />
-              {/* <h1 onClick={()=>alert(currentUser)}>Click for state</h1> */}
-              <p className='recent-spots'>Recently added</p>
-              <div id='spots-grid' className={spotList.length === 0 ? '' : 'spots-grid'}>                                                                                
-                {(searchText === '' || searchText === undefined) ? 
-                <DisplaySpots list={spotList} /> : 
-                <DisplaySpots list={spotList.filter(spot => {
-                  return (spot.title.toLowerCase().includes(searchText.toLowerCase()) || spot.description.toLowerCase().includes(searchText.toLowerCase()));
-                })} />}
-              </div>
-              <Link to='/the-spot/add-spot'><AddSpotButton /></Link>
+              <Spots />
             </Route>
             <Route path='/the-spot/add-spot'>
               <AddSpot />
@@ -80,7 +31,6 @@ export default function App() {
               <Register />
             </Route>
           </Switch>
-        </div>
       </Router>
     );
 }
---
+++ src/components/Spots/Spots.js
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react';
+import SpotCard from './components/SpotCard/SpotCard';
+import AddSpotButton from './components/AddSpotButton/AddSpotButton';
+import NavBar from './components/NavBar/NavBar';
+import { Link, useHistory } from 'react-router-dom';
+import { setSpotList } from './actions';
+import { useSelector, useDispatch } from 'react-redux';
+
+const Spots = _ => {
+    const spotList = useSelector(state => state.modifySpotList.spotList);
+    const searchText = useSelector(state => state.changeSearch.searchText);
+    const currentUser = useSelector(state => state.changeCurrentUser.currentUserEmail);
+    const history = useHistory();
+    const dispatch = useDispatch();
+  
+    useEffect(() => {
+      if(!currentUser) {
+        console.log('No current user detected, redirecting to sign in');
+        if(history)
+          history.push('/the-spot/sign-in');
+      }
+      if(currentUser) {
+        fetch(`https://the-spot01.herokuapp.com/get-spots/${currentUser}`)
+        .then(response => response.json())
+        .then(data => {
+          dispatch(setSpotList(data));
+        })
+        .catch(err => console.log(err))
+      }
+    })
+
+    const DisplaySpots = props => {
+        if (props.list.length === 0) {
+            return <p style={{ textAlign: 'center', marginTop: '15%', fontSize: '1.5rem' }}>No spots found, <Link to='/the-spot/add-spot' style={{ color: 'black' }}><strong>add one now!</strong></Link></p>
+        } else if (props.list.length > 0) {
+            return props.list.map(spot => {
+                return (
+                    <SpotCard id={spot.id} title={spot.title} description={spot.description} image={spot.image} coords={spot.coords} dateCreated={spot.dateCreated} />
+                );
+            })
+        } else {
+            return null;
+        }
+    }
+
+    return (
+        <div>
+            <NavBar />
+            <p className='recent-spots'>Recently added</p>
+            <div id='spots-grid' className={spotList.length === 0 ? '' : 'spots-grid'}>
+                {(searchText === '' || searchText === undefined) ?
+                    <DisplaySpots list={spotList} /> :
+                    <DisplaySpots list={spotList.filter(spot => {
+                        return (spot.title.toLowerCase().includes(searchText.toLowerCase()) || spot.description.toLowerCase().includes(searchText.toLowerCase()));
+                    })} />}
+            </div>
+            <Link to='/the-spot/add-spot'><AddSpotButton /></Link>
+        </div>
+    );
+}
+
+export default Spots;
